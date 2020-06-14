5dc411a015b6f0603e95b3c2a224c10dd1d38212
Zenahr Barzani <zenmatica@gmail.com>
2020-06-14 04:37:04 +0200

    added stringBuilder and general dataFlow design

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ dict({
     pass
 
 
-def notationFinder(chordName):
-    """Finds the right numeric chord notation and returns a string
+def chordNotationPairer(chordName):
+    """Finds the right numeric chord notation and returns a tuple of (chordName, notation)
 
     Args:
         chordName (string): Example: GM7
@@ -30,34 +30,44 @@ def notationFinder(chordName):
     Return:
         Example:
             I -> Cm9
-            O -> x3x133x
+            O -> ('Cm9', 'x3x133x')
     """
 
-    if (chordName == 'Gmaj7'):
-        return '355433'
+    if (normaliseChordName) == 'Gmaj7'):
+        return ('Gmaj7', '355433')
 
-    elif (chordName == 'Dmaj7'):
-        return 'x57675'
+    elif (normaliseChordName) == 'Dmaj7'):
+        return ('Dmaj7', 'x57675')
 
-    elif (chordName == 'F#7b13'):
-        return 'x57675'
+    elif (normaliseChordName) == 'F#7b13'):
+        return ('F#7b13', 'x57675')
 
-    elif (chordName == 'C#7#9' or chordName == 'C#7(#9)'):
-        return 'x57675'
+    elif (normaliseChordName) == 'C#7#9'):
+        return ('C#7#9', 'x57675')
 
-    elif (chordName == 'A13'):
-        return 'x57675'
+    elif (normaliseChordName) == 'A13'):
+        return ('A13', 'x57675')
 
-    elif (chordName == 'B79'):
-        return 'x57675'
+    elif (normaliseChordName) == 'B79'):
+        return ('B79', 'x57675')
 
-    elif (chordName == 'Em9'):
-        return 'x57675'
+    elif (normaliseChordName) == 'Em9'):
+        return ('Em9', 'x57675')
 
-    elif (chordName == 'Bm9'):
-        return 'x57675'
+    elif (normaliseChordName) == 'Bm9'):
+        return ('Bm9', 'x57675')
 
 def normaliseChordName(chordName):
     chordName = chordName.replace('(', '')
     chordName = chordName.replace(')', '')
     return chordName
+
+
+def stringBuilder(chordStream):
+    string = ''
+    string.join([for (chordName, notation) in chordStream, rowBuilder(chordName, notation)]) # -> [[row], ..., [row]]
+
+def numericNocationBuilder(userInput):
+    chordStream = inputDissector(userInput) # Create list of chord names by seperating string into items via " " or "," or ", " delimeter.
+    chordStream = chordStreamFeeder(chordStream, chordNotationPairer()) # Callback function mapping every entry of the chordStream list to the callback function.
+    chordStream = stringBuilder(chordStream) # chordStream -> [(chordName, chordNotation)] -> String with carriage returns.
---
+++ tests/app.py
@@ -0,0 +1,22 @@
+""""Assert stuff as follows
+
+chordNotationPairer()
+------------------------------------------------------
+I -> "Gmaj7"
+Assert O to be ->
+"('Gmaj7', '355433')
+"
+------------------------------------------------------
+
+numericNocationBuilder(input):
+input = F#7(b13) Em9 Dmaj7(9) C#7(#9) Bm9 A13 B7(9)
+Assert O ->
+
+"Gmaj7=355433\n
+...
+Em9=______\n
+...
+B7(9)=______
+"
+
+"""
