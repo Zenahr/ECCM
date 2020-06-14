112082b75c9dbd0241ac64a304f9291310d82db5
Zenahr Barzani <zenmatica@gmail.com>
2020-06-14 04:59:35 +0200

    added chord name alias recognition test draft

--- tests/app.py
+++ tests/app.py
@@ -1,3 +1,49 @@
+dataset = {
+    "inputVerification": [
+    "",
+    " ",
+    "&", # stop when non alphanumerical values are read from inputStream
+    "  1123josbiosdcio!!! ", # non AlphaNum check should catch again
+    "Gmaj7",
+    "GM7",
+    "G7"
+    ],
+
+    "chordIdentification": [
+        {
+        "GM7": [
+            "GM7",
+            "Gmaj7",
+            "GM",
+            "Gmaj",
+        ],
+        "DM7": [
+            # Alias
+        ],
+        "F#7(b13)": [
+            # Alias
+        ],
+        "C#7(#9)": [
+            # Alias
+        ],
+        
+        "A13": [
+            # Alias
+        ],
+        "B7": [
+            # Alias
+        ],
+        "Em9": [
+            # Alias
+        ],
+        "Bm9": [
+            # Alias
+        ],
+
+        }
+    ]
+}
+
 """"Assert stuff as follows
 
 chordNotationPairer()
