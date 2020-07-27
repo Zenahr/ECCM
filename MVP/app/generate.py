c50794b3f76d392bd712aa28695d56bd83ff73d9
Zenahr Barzani <zenmatica@gmail.com>
2020-07-27 22:02:41 +0200

    added lookup table logic

--- MVP/app/generate.py
+++ MVP/app/generate.py
@@ -3,7 +3,7 @@ from json import load
 import yaml
 
 template_env = Environment(loader=FileSystemLoader(searchpath='./template'))
-template = template_env.get_template('source.txt')
+template = template_env.get_template('template.txt')
 
 with open('template/config.yaml', 'r') as config_file:
     config = yaml.load(config_file)
@@ -11,15 +11,32 @@ with open('template/config.yaml', 'r') as config_file:
 with open('output/song.txt', 'w') as output_file:
     output_file.write(
         template.render(
-            title=config['content']['title'],
-            greetingText=config['content']['greetingText'],
-            resumeText=config['content']['resumeText'],
-            firstName=config['content']['firstName'],
-            lastName=config['content']['lastName'],
-            github=config['socials']['github'],
-            linkedIn=config['socials']['linkedIn'],
-            email=config['socials']['email'],
-            socialIntroText=config['content']['socialIntroText'],
-            closingStatement=config['content']['closingStatement'],
+            songTitle=config['songTitle'],
+            chords=chordBuilder(config['chords']),
         )
     )
+
+def chordBuilder(chord_stream):
+    """
+    Splits input into seperate chords and returns numeric chord diagram for each chord. Returns string
+    """
+    result = ""
+    split_stream = chord_stream.spilt()
+    result = chordBuilderHelper(split_stream)
+
+def chordBuilderHelper(split_stream):
+    result = ""
+    for chord in split_stream:
+        chord = lookUpChord(chord)
+    return split_stream
+
+def lookUpChord(chordName, delimeter="::"):
+    lookUpTable = {
+        "Am7": "5x555x",
+        "D9": "x5455x",
+        "Gmaj7": "3x443x",
+    }
+    if chordName in lookUpTable:
+        return chordName + delimeter + lookUpTable[chord]
+    else:
+        return "N/A"
